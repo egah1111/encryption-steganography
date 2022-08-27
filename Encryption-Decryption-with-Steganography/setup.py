6b316e46b8e023d6c22c8808451ee833f21c3bb1
Egah Francis <108225524+egah1111@users.noreply.github.com>
2022-08-27 02:18:32 +0100

    Add files via upload

---
+++ Encryption-Decryption-with-Steganography/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encrption-decription-with-steganography',
+    version='0.2',
+    packages=find_packages(),
+    license='MIT',
+    description='Encryption and decryption with steganography modified',
+    long_description=open('README.md').read(),
+    install_requires=['flask'],
+    url='https://github.com/<egah1111>/<encrption-decription-with-steganography>',
+    author='<Egah Francis>',
+    author_email='<egahhenry@gmail.com'
+)
